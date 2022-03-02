4490443dca5fc166373ecfe03b4d2bcba1cea07b
Ian von Pechmann <ianyvonpechmann@gmail.com>
2022-03-02 10:33:24 -0700

    put home into main

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,9 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:jade/routes.dart';
 import 'package:jade/theme.dart';
 import 'firebase_options.dart';
-import 'screens/home.dart';
+import 'services/auth.dart';
+import 'screens/feed.dart';
+import 'screens/landing.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,7 +16,6 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,5 +25,51 @@ class MyApp extends StatelessWidget {
       theme: themeData,
       home: const Home()
     );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+        stream: AuthService().userStream,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.active) {
+            if (snapshot.hasData && AuthService().user!.emailVerified) {
+              FutureBuilder(
+                future: AuthService().profileExists(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.active && snapshot.hasData) {
+                    return const FeedScreen();
+                  } else if (snapshot.hasError) {
+                    return Center(
+                      child: Text('${snapshot.error}'),
+                    );
+                  } else if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  } else {
+                    return const LandingScreen();
+                  }
+                }
+              );
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Text('${snapshot.error}'),
+              );
+            }
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return const LandingScreen();
+        });
   }
 }
